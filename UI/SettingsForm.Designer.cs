4f82fc03d738326a4556c118fbb7c641eb336ea0
bax082024 <bax082024@gmail.com>
2024-12-27 16:04:50 +0100

    added numericupdowns

--- UI/SettingsForm.Designer.cs
+++ UI/SettingsForm.Designer.cs
@@ -29,16 +29,37 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            numMinShuffleCount = new NumericUpDown();
+            numShuffleSpeed = new NumericUpDown();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numMinShuffleCount).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numShuffleSpeed).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(numShuffleSpeed);
+            panel1.Controls.Add(numMinShuffleCount);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 683);
             panel1.TabIndex = 0;
+            // 
+            // numMinShuffleCount
+            // 
+            numMinShuffleCount.Location = new Point(308, 133);
+            numMinShuffleCount.Name = "numMinShuffleCount";
+            numMinShuffleCount.Size = new Size(120, 23);
+            numMinShuffleCount.TabIndex = 0;
+            // 
+            // numShuffleSpeed
+            // 
+            numShuffleSpeed.Location = new Point(308, 182);
+            numShuffleSpeed.Name = "numShuffleSpeed";
+            numShuffleSpeed.Size = new Size(120, 23);
+            numShuffleSpeed.TabIndex = 1;
             // 
             // SettingsForm
             // 
@@ -51,11 +72,16 @@
             Name = "SettingsForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Settings";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numMinShuffleCount).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numShuffleSpeed).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private NumericUpDown numShuffleSpeed;
+        private NumericUpDown numMinShuffleCount;
     }
 }
