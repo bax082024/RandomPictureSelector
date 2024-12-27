4289eeb4db353f14a5f3eedd9b17be46c579a470
bax082024 <bax082024@gmail.com>
2024-12-27 11:32:21 +0100

    added statusstrip + logo + main panel

--- RandomForm.Designer.cs
+++ RandomForm.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            MainPanel = new Panel();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            MainPanel.SuspendLayout();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            MainPanel.BackColor = Color.Transparent;
+            MainPanel.Controls.Add(statusStrip1);
+            MainPanel.Dock = DockStyle.Fill;
+            MainPanel.Location = new Point(0, 0);
+            MainPanel.Name = "MainPanel";
+            MainPanel.Size = new Size(1009, 669);
+            MainPanel.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 647);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1009, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(74, 17);
+            toolStripStatusLabel1.Text = "Bax Creation";
             // 
             // RandomForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightYellow;
-            ClientSize = new Size(949, 602);
+            ClientSize = new Size(1009, 669);
+            Controls.Add(MainPanel);
             Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "RandomForm";
             Text = "Random Picture Selector";
+            MainPanel.ResumeLayout(false);
+            MainPanel.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel MainPanel;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
